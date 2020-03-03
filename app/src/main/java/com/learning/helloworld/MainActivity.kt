9024cf089f6c1e8caff4bc307cfb673bc231e6e4
Ernest Asanov <ern.asanov@yandex.ru>
2020-03-03 20:41:37 +0300

    Login screen and notification

--- app/src/main/java/com/learning/helloworld/MainActivity.kt
+++ app/src/main/java/com/learning/helloworld/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.learning.helloworld
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.core.app.NotificationManagerCompat
+import com.learning.helloworld.LoginActivity.Companion.KEY_LOGIN
 
 class MainActivity : AppCompatActivity() {
     private var button : Button? = null
@@ -13,12 +19,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val login = intent.extras?.get(KEY_LOGIN)
         val t = findViewById<TextView>(R.id.textView)
-        val b = findViewById<Button>(R.id.button)
-
-        b.setOnClickListener {
-            val intent = Intent(this, ChildActivity::class.java)
-            startActivity(intent)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            val channel = NotificationChannel("notification_channel", "channel_01", NotificationManager.IMPORTANCE_DEFAULT)
+            notificationManager.createNotificationChannel(channel)
+            val notification = Notification.Builder(this, channel.id)
+                .setSmallIcon(R.drawable.person_icon)
+                .setContentTitle("Test notification")
+                .build()
+            notificationManager.notify(12, notification)
         }
     }
 }
